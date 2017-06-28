1be1a9329ef22f7b9ff657c3ad080f5097f86c34
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-06-28 02:01:07 -0300

    Turns Configuration into a class

--- simple-mock-server.py
+++ simple-mock-server.py
@@ -2,45 +2,45 @@
 import BaseHTTPServer
 import json
 import time
+import httplib
 
-
-def load_configuration_file():
-    file_name = 'simple_mock_server_conf.json'
-    configuration = {}
-    with open(file_name) as conf_file:
-        json_config = json.loads(conf_file.read())
-        configuration['hostname'] = json_config.get('hostname')
-        configuration['port'] = json_config.get('port')
-
-    return configuration
+class Configuration(object):
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
 
 class SimpleHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(self):
-        self.send_response(200)
+        self.send_response(httplib.OK)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def do_GET(self):
         print 'Context: %s' % self.path
-        self.send_response(200)
+        self.send_response(httplib.OK)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write('')
 
     def do_POST(self):
         print 'Context: %s' % self.path
-        self.send_response(200)
+        self.send_response(httplib.OK)
 
+def load_configuration_file():
+    file_name = 'simple_mock_server_conf.json'
+    with open(file_name) as conf_file:
+        json_config = json.loads(conf_file.read())
+        configuration = Configuration(json_config.get('hostname'), json_config.get('port'))
+
+    return configuration
 
 if __name__ == '__main__':
     config = load_configuration_file()
-    hostname = config.get('hostname')
-    port = config.get('port')
 
     server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((hostname, config.get('port')), SimpleHandler)
+    httpd = server_class((config.hostname, config.port), SimpleHandler)
 
-    print time.asctime(), "Server Starts - %s:%s" % (hostname, port)
+    print time.asctime(), "Server Starts - %s:%s" % (config.hostname, config.port)
 
     try:
         httpd.serve_forever()
@@ -48,4 +48,4 @@ if __name__ == '__main__':
         pass
 
     httpd.server_close()
-    print time.asctime(), 'Server Stops - %s:%s' % (hostname, port)
+    print time.asctime(), 'Server Stops - %s:%s' % (config.hostname, config.port)
