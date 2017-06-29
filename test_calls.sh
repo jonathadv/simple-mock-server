be5c4b9bfb610a7a3f8afb69909dec6e9dcd6d12
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-06-29 01:59:32 -0300

    Adding script to test server using curl

---
+++ test_calls.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+config_file=simple_mock_server_conf.json
+
+if [[ ! -f "${config_file}" ]]; then
+    echo "file ''${config_file}' not found. Nothing to do. Exiting."
+    exit 1
+fi
+
+echo "Reading configuration file '${config_file}'..."
+
+host=$(egrep '"hostname"' ${config_file} | cut -d ':' -f 2 | sed 's/[",]//g')
+port=$(egrep '"port":[0-9]+' ${config_file} | cut -d ':' -f 2 | sed 's/[^0-9]//g' | sed 's/[^0-9]//g')
+
+if [[ -z  "${host}" || -z "${port}" ]]; then
+    echo "Unable to get host or port from file '${config_file}''. Nothing to do. Exiting."
+    exit 1
+fi
+
+echo "Testing calls against http://${host}:${port}..."
+echo
+
+curl -X GET http://${host}:${port}/status; echo
+curl -X POST http://${host}:${port}/add; echo
+curl -X PUT http://${host}:${port}/update; echo
+curl -X DELETE http://${host}:${port}/remove; echo
+echo
+curl -I -X GET http://${host}:${port}/redirect; echo
